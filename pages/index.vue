d2eefed506eaf501c7a508ef6c4d9b4e29107022
donald.m <donald.m@glotelho.com>
2024-11-29 15:20:31 +0100

    Added hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        <h1 class="text-xl font-500 h-screen">Content</h1>
+        <section class="text-gray-600">
+            <div class="container mx-auto flex px-5 py-20 min-h-80 max-w-6xl items-center justify-center flex-col">
+                <div class="text-center lg:w-2/3 w-full">
+                    <h1 class="font-extrabold sm:text-4xl text-3xl mb-4 text-gray-900">Modern UI Components built with Tailwind CSS on Vue, React & Angular </h1>
+                    <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
+                        tousled. </p>
+                    <div class="flex justify-center">
+                        <button
+                            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                        <button
+                            class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
